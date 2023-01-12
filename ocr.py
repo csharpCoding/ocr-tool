daeacee666c1c2cf6b8ac92a23c8207ae73ec92b
csharpCoder <33175349+csharpCoding@users.noreply.github.com>
2023-01-12 21:46:58 +0900

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -16,7 +16,7 @@ async def read_image(img_path, lang='eng'):
 
     pytesseract.pytesseract.tesseract_cmd = '/app/vendor/tesseract-ocr/bin/tesseract'
     try:
-        return pytesseract.image_to_string(img_path, lang='lets', config='--psm 6')
+        return pytesseract.image_to_string(img_path, lang=lang)
     except:
         return "[ERROR] Unable to process file: {0}".format(img_path)
 
